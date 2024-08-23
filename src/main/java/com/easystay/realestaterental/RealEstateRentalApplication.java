cc6aa995d15277d8eb9a9815f312a0e120b577bf
Nabil Sabi <nabilsabi01@gmail.com>
2024-08-23 18:38:53 +0100

    Création de la structure du projet Backend Spring Boot

---
+++ src/main/java/com/easystay/realestaterental/RealEstateRentalApplication.java
@@ -0,0 +1,13 @@
+package com.easystay.realestaterental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easystay/realestaterental/RealEstateRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easystay.realestaterental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
